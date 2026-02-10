7169614aa1568f8ede2c6d61ad9ffc2c8d92401d
Aloof <johnharvycastillo@gmail.com>
2026-02-10 16:24:33 +0800

    refactor: consolidate bot logic into index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 require('./main');
 
-const path = require('path');
-const express = require("express");
-const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => {
-    const imagePath = path.join(__dirname, 'index.html');
-    res.sendFile(imagePath);
-});
-
-app.listen(port, '0.0.0.0', () => {
-    console.log(`🔥 Weedify Bot - Listening on port ${port}`);
-});
+require('./main');
